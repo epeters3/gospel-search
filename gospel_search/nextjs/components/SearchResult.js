93ae194bdba877a4fe56968ef2d57f394571a09b
epeters3 <evanpeterson17@gmail.com>
2020-04-06 13:51:56 -0600

    Add document type label to search results

--- gospel_search/nextjs/components/SearchResult.js
+++ gospel_search/nextjs/components/SearchResult.js
@@ -2,19 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin-top: ${p => p.theme.size.xl};
+  margin-top: ${(p) => p.theme.size.xl};
 `;
 
 const H4 = styled.h4`
-  margin-bottom: ${p => p.theme.size.xs};
+  margin-bottom: ${(p) => p.theme.size.xs};
 `;
 
 const Text = styled.p`
-  margin: ${p => p.theme.size.xs} 0;
+  margin: ${(p) => p.theme.size.xs} 0;
 `;
 
 const VerseNumber = styled.b`
-  margin-right: ${p => p.theme.size.xs};
+  margin-right: ${(p) => p.theme.size.xs};
 `;
 
 const SearchResult = ({
@@ -28,18 +28,22 @@ const SearchResult = ({
   score,
   talk_id,
   text,
-  year
+  year,
 }) => (
   <Container>
+    <H4>
+      <a href={parent_id}>
+        {doc_type == "scriptures" ? `${name}:${num}` : name}
+      </a>{" "}
+    </H4>
+
     <Text>
-      <H4>
-        <a href={parent_id}>
-          {doc_type == "scriptures" ? `${name}:${num}` : name}
-        </a>
-      </H4>
       {doc_type == "scriptures" ? <VerseNumber> {num}</VerseNumber> : null}
       {text}
     </Text>
+    <i>
+      [{doc_type === "scriptures" ? "Scripture" : `${year} Conference Talk`}]
+    </i>
   </Container>
 );
 
