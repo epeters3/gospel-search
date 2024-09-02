b56febb4605654f6db5cb2010d467b548d717162
Evan Peterson <evanpeterson17@gmail.com>
2024-09-02 14:30:01 -0600

    Migrate QA route to new Python back-end

--- gospel_search/api/main.py
+++ gospel_search/api/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from gospel_search.api.types import AnswerResult
 from gospel_search.chroma.client import Chroma
 from gospel_search.utils import logger
+from gospel_search.llm.main import qa_chain
 
 app = FastAPI(name="Gospel Search API")
 logger.info("Creating chroma client...")
@@ -16,8 +17,8 @@ def search(query: str, k: int = 10):
 
 @app.get("/api/qa")
 def qa(query: str):
-    # TODO: implement
-    return AnswerResult(answer="this is your answer.")
+    answer = qa_chain.invoke({"query": query})
+    return AnswerResult(answer=answer)
 
 # TODO: this only works when the user first visits the root page. It doesn't work when a user
 # first visits one of the other pages.
--- gospel_search/llm/main.py
+++ gospel_search/llm/main.py
@@ -1,8 +1,32 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers.string import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import chain
+
+from gospel_search.api.types import SearchResult
+from gospel_search.chroma.client import Chroma
+    
+
+def stringify_search_result(result: SearchResult) -> str:
+    segment = result.segment
+    return f"""Reference name: {segment.name}
+Text: ${segment.text}
+Link: ${segment.parent_id}
+"""
 
 llm = ChatOpenAI(model='gpt-4o-2024-08-06')
+
+def Retriever(*, k: int):
+    chroma = Chroma()
+
+    @chain
+    def retrieve(input):
+        res = chroma.search(input["query"], k)
+        return "\n".join([stringify_search_result(r) for r in res.results])
+    
+    return retrieve
+
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -19,8 +43,8 @@ Related scripture verses and conference talk paragraphs:
 {results}
 """,
         ),
-        ("human", "{input}"),
+        ("human", "{query}"),
     ]
 )
 
-qa_chain = prompt | llm | StrOutputParser
+qa_chain = {"query": RunnablePassthrough(), "results": Retriever(k=10)} | prompt | llm | StrOutputParser()
